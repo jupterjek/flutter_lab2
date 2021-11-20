23bc21d2380a32528688030a559e87b5621424fe
FlutLab.io <>
2021-11-20 04:56:23 +0000

    Autosave

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        backgroundColor: Colors.teal,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Text(
+          "Home Page",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_right),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
